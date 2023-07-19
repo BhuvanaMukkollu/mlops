223db8af9bae5f758297bcc1c0633ce78f19ea6a
BhuvanaMukkollu <136561752+BhuvanaMukkollu@users.noreply.github.com>
2023-07-19 10:40:36 +0530

    Update Daimond_Price_predection.py

--- Daimond_Price_predection.py
+++ Daimond_Price_predection.py
@@ -1,52 +1,87 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-# Expander section
-with st.expander("About"):
-  st.write("""Trying to add a data table, chart, sidebar button with 
-          ballons, an image, text input & exploring tabs!""")
+import pickle
+#with open('model.pkl', 'rb') as file:
+    #model = pickle.load(file)
+with open(" model.pkl", 'rb') as file:  
+    model=pickle.load(file)
 
-# Sidebar section
-with st.sidebar:
-  st.subheader('This is a Sidebar')
-  st.write('Button with Balloons 🎈')
-  if st.button('Click me!✨'):
-    st.balloons()
-  else:
-    st.write(' ')
-
-# Dataframe and Chart display section
-st.subheader('Interactive Data Table')
-df = pd.DataFrame(
-    np.random.randn(50, 3),  # generates random numeric values!
-    columns=["a", "b", "c"])
-st.dataframe(df) 
-
-st.subheader('Bar Chart 📊')
-st.bar_chart(df)
-
-# Image upload and text input section
-st.subheader('An Image')
-st.image('https://www.scoopbyte.com/wp-content/uploads/2019/12/tom-and-jerry.jpg')
-
-st.subheader('Text Input')
-greet = st.text_input('Write your name, please!')
-st.write('👋 Hey!', greet)
+st.title("Diamond Price Prediction: Input Data")
 
 
-# Tabs section
-st.subheader('Tabs')
-tab1, tab2 = st.tabs(["TAB 1", "TAB 2"])
 
-with tab1:
-  st.write('WOW!')
-  st.image("https://i.gifer.com/DJR3.gif", width=400)
+# Text Field
+st.subheader("Carat")
+carat = st.text_input("Enter Carat")
+st.write("Carat:", carat)
+st.subheader("Depth")
+depth = st.text_input("Enter Depth")
+st.write("Depth:", depth)
 
-with tab2:
-  st.write('Do you like ice cream? 🍨')
-  agree = st.checkbox('Yes! I love it')
-  disagree = st.checkbox("Nah! 😅")
-  if agree:
-    st.write('Even I love it 🤤')
-  if disagree:
-    st.write('You are boring 😒')
+
+#st.write("Volume:",float(x)*float(y)*float(z))
+
+
+# Radio Buttons
+st.subheader("Cut")
+cut = st.radio("Select Cut value", ["Fair","Good","Very Good","Premium","Ideal"])
+st.write("Cut value:", cut)
+if cut=="Fair": 
+    cut=1
+elif cut=="Good":
+    cut=2
+elif cut=="Very Good":
+    cut=3
+elif cut=="Premium":
+    cut=4
+elif cut=="Ideal":
+    cut=5
+st.subheader("Color")
+color = st.radio("Select color", ["D","E","F","G","H","I","J"])
+st.write("Color:", color)
+if color=="J": 
+    color=1
+elif color=="I":
+    color=2
+elif color=="H":
+    color=3
+elif color=="G":
+    color=4
+elif color=="F":
+    color=5
+elif color=="E":
+    color=6
+elif color=="D":
+    color=7
+
+st.subheader("Clarity")
+clarity = st.radio("Select Clarity", ["I1","SI2","SI1","VS2","VS1","VVS2","VVS1","IF"])
+st.write("Clarity Level:", clarity)
+if clarity=="I1": 
+    clarity=1
+elif clarity=="SI2":
+    clarity=2
+elif clarity=="SI1":
+    clarity=3
+elif clarity=="VS2":
+    clarity=4
+elif clarity=="VS1":
+    clarity=5
+elif clarity=="VVS2":
+    clarity=6
+elif clarity=="VVS1":
+    clarity=7
+elif clarity=="IF":
+    clarity=8
+
+
+# Button
+#st.subheader("Button Example")
+if st.button("Predict Price"):
+    st.write("Price Predicted!")
+    yhat_test = model.predict([[carat,cut,color,clarity,depth]])
+    st.write("Diamond Price is $",yhat_test)
+# Display Messages
+#st.success("Success message")
+#st.info("Info message")
+#st.warning("Warning message")
+#st.error("Error message")
